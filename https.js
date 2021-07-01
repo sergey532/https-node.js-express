47d7749c51b48564025121df22f00a9541e20feb
sergey532 <82769145+sergey532@users.noreply.github.com>
2021-07-01 13:36:46 +0300

    Update https.js
    Вариант 4

--- https.js
+++ https.js
@@ -58,4 +58,24 @@ https.createServer({
   console.log('Example app listening on port 3000! Go to https://localhost:3000/')
 })
 
+//Вариант 4
+//Generate cert.pem and key.pem
+//openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365
+
+const app = require('express')();
+const https = require('https');
+const fs = require('fs');
+
+//GET home route
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+// we will pass our 'app' to 'https' server
+https.createServer({
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem'),
+    passphrase: 'YOUR PASSPHRASE HERE'
+}, app)
+.listen(3000);
 
