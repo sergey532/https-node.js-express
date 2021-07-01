21478941be1aba8b1f0cb563b03f303cefc0dd8f
sergey532 <82769145+sergey532@users.noreply.github.com>
2021-07-01 13:14:49 +0300

    Update https.js
    Вариант 2

--- https.js
+++ https.js
@@ -15,3 +15,21 @@ httpsOptions = {
 https.createServer(httpsOptions, app).listen(443);
 
 //*app – это объект полученый от express().
+
+//Вариант 2
+
+const https = require('https');
+const fs = require('fs');
+
+// readFileSync function must use __dirname get current directory
+// require use ./ refer to current directory.
+
+const options = {
+   key: fs.readFileSync(__dirname + '/private.key', 'utf8'),
+  cert: fs.readFileSync(__dirname + '/public.cert', 'utf8')
+};
+
+
+ // Create HTTPs server.
+
+ var server = https.createServer(options, app);
